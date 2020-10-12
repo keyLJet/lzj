d5cbdb0eca236c54f3de8a2b8f4d83c242f03849
keyLJet <keyLJet@hotmail.com>
2020-10-12 16:51:55 +0800

    20201012

---
+++ day02/.history/14.异步写入回调地狱解决2_20201011233949.js
@@ -0,0 +1,21 @@
+
+
+const fs = require("fs");
+const path = require("path");
+const {
+    promisify
+} = require("util");
+const filePath = path.resolve(__dirname, "./01.txt");
+const openFile = promisify(fs.open);
+const writeFile = promisify(fs.open);
+const closeFile = promisify(fs.close);
+const fn = async () => {
+     const fd = await openFile(filePath,"a");
+     await writeFile(fd,"wolegequ");
+     await closeFile(fd);
+ };
+ fn().then(() => {
+     console.log("xiehaole")
+ }).catch((err) => {
+     console.log(err)
+ })
---
+++ lzj/.history/lzjserver_20201012161324.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const server = http.createServer((request, response) => {
+    response.setHeader("Content-Type", "text/html;charset=utf-8")
+    //返回相应
+    response.end("<h1>湖人总冠军</h1>");
+})
+const port = 3000;
+const host = "192.168.20.25";
+server.listen(port, host, (err) => {
+    if (err) {
+        console.log("服务器启动失败：" + err);
+        return;
+    }
+    console.log(`服务器启动成功:请访问 http://${host}:${port}`)
+})
